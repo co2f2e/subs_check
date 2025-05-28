c46a55945352764953e2837d06c35024f3821dcc
co2f2e <48397609+co2f2e@users.noreply.github.com>
2025-05-29 04:33:25 +0800

    Update download_source.sh

--- bash/download_source.sh
+++ bash/download_source.sh
@@ -40,5 +40,26 @@ cd "$SUBS_DIR" || exit 1
 
 go build -ldflags="-X main.Version=dev -X main.CurrentCommit=local" -o subs-check
 
-echo " subs-check 编译成功，运行方法如下："
-echo "./subs-check -f config/config.yaml"
+echo " subs-check 编译成功，执行方式./subs-check -f config/config.yaml"
+
+get_tcp_port() {
+  local port
+  port=$(devil port list | awk '$1=="tcp" {print $3; exit}')
+  if [ -n "$port" ]; then
+    echo "$port"
+  else
+    devil port add tcp random >/dev/null 2>&1
+    port=$(devil port list | awk '$1=="tcp" {print $3; exit}')
+    echo "$port"
+  fi
+}
+
+tcp_port=$(get_tcp_port)
+echo "分配到的 TCP 端口是: $tcp_port"
+
+if [ -f "$CONFIG_FILE" ]; then
+  sed -i '' "s|^listen-port: \".*\"|listen-port: \":$tcp_port\"|" "$CONFIG_FILE"
+  echo "已更新 $CONFIG_FILE 中的 listen-port 为 :$tcp_port"
+else
+  echo "配置文件 $CONFIG_FILE 不存在"
+fi
