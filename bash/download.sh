9ecb0715339e062ffe446f52954c381073d2a904
co2f2e <48397609+co2f2e@users.noreply.github.com>
2025-05-28 21:26:24 +0800

    Update download.sh

--- bash/download.sh
+++ bash/download.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 clear
-cd /
 
-SUBS_DIR="subs_check"
+SUBS_DIR="/subs_check"
 BINARY_NAME="subs-check"
-CRON_CMD="cd /$SUBS_DIR && ./subs-check > subs-check.log 2>&1 &"
+BINARY_PATH="$SUBS_DIR/$BINARY_NAME"
+LOG_PATH="$SUBS_DIR/$BINARY_NAME.log"
+CRON_CMD="$BINARY_PATH > $LOG_PATH 2>&1 &"
 
 if [ ! -d "$SUBS_DIR" ]; then
-  mkdir "$SUBS_DIR"
+  mkdir -p "$SUBS_DIR"
   cd "$SUBS_DIR" || exit 1
 
   curl -L -o ${BINARY_NAME}_Linux_x86_64.tar.gz $(curl -s https://api.github.com/repos/beck-8/subs-check/releases/latest \
@@ -17,15 +18,14 @@ if [ ! -d "$SUBS_DIR" ]; then
   && tar -xzf ${BINARY_NAME}_Linux_x86_64.tar.gz \
   && rm ${BINARY_NAME}_Linux_x86_64.tar.gz
 
-  chmod +x "$BINARY_NAME"
-  echo "下载并解压成功，请修改配置文件 config.yaml 然后执行下面命令后台运行二进制文件，日志文件位于 subs-check.log "
-  echo "nohup ./subs-check > output.log 2>&1 &"
+  chmod +x "$BINARY_PATH"
+  echo "下载并解压成功，请修改配置文件 config.yaml 并执行后台运行命令运行。"
 else
   cd "$SUBS_DIR" || exit 1
-  echo "目录已存在，跳过下载解压"
+  echo "目录已存在，请先手动删除"
 fi
 
-crontab -l 2>/dev/null | grep -F "$CRON_CMD" >/dev/null
+crontab -l 2>/dev/null | grep -F "@reboot $CRON_CMD" >/dev/null
 if [ $? -ne 0 ]; then
   (crontab -l 2>/dev/null; echo "@reboot $CRON_CMD") | crontab -
   echo "已添加到开机自启任务"
